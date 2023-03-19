3fb1c8836c269fa85c04856a7e30d1ae209b3bf6
elisabeth <elisabeth.r2ni@gmail.com>
2023-03-19 21:56:55 +0200

    Lisa peaklass Kaart vajalike isendiväljadega

---
+++ src/src/Kaart.java
@@ -0,0 +1,42 @@
+public class Kaart implements Visiitkaart{
+
+    private String nimi;
+    private String email;
+    private String telefoniNumber;
+
+    public Kaart(String nimi, String email, String telefoniNumber) {
+        this.nimi = nimi;
+        this.email = email;
+        this.telefoniNumber = telefoniNumber;
+    }
+
+    @Override
+    public String getNimi() {
+        return nimi;
+    }
+
+    @Override
+    public void setNimi(String uusNimi) {
+        this.nimi = uusNimi;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public void setEmail(String uusEmail) {
+        this.email = uusEmail;
+    }
+
+    @Override
+    public String getTelefoniNumber() {
+        return telefoniNumber;
+    }
+
+    @Override
+    public void setTelefoniNumber(String uusTelefoniNumber) {
+        this.telefoniNumber = uusTelefoniNumber;
+    }
+}
