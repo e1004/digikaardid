07a1f0ef89cad13c57c1968cbcff2d2662d44e31
elisabeth <elisabeth.r2ni@gmail.com>
2023-03-19 21:56:20 +0200

    Lisa Visiitkaart get/set meetoditega

--- src/src/Visiitkaart.java
+++ src/src/Visiitkaart.java
@@ -1,2 +1,13 @@
 public interface Visiitkaart {
+    String getNimi();
+
+    void setNimi(String nimi);
+
+    String getEmail();
+
+    void setEmail(String uusEmail);
+
+    String getTelefoniNumber();
+
+    void setTelefoniNumber(String uusTelefoniNumber);
 }
