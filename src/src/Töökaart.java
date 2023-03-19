2edcc232c951a437bb8536184c2383ba900edcf6
elisabeth <elisabeth.r2ni@gmail.com>
2023-03-19 21:58:00 +0200

    Muuda Töökaart alamklassiks

--- src/src/Töökaart.java
+++ src/src/Töökaart.java
@@ -1,2 +1,5 @@
-public class Töökaart extends DialoogiHaldur{
+public class Töökaart extends Kaart{
+    public Töökaart(String nimi, String email, String telefoniNumber) {
+        super(nimi, email, telefoniNumber);
+    }
 }
