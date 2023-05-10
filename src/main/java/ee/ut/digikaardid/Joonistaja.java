776e7cc9ec9bdc82f842ca8a3862dfb18aefb643
elisabeth <elisabeth.r2ni@gmail.com>
2023-05-10 21:36:21 +0300

    Kustuta väljakommenteeritud kood

--- src/main/java/ee/ut/digikaardid/Joonistaja.java
+++ src/main/java/ee/ut/digikaardid/Joonistaja.java
@@ -26,19 +26,12 @@ public class Joonistaja {
         String rida = kaart.toString();
         String[] osad = rida.trim().split(", ");
 
-        /*for (int i = 2; i < (kaardiKülg - osad.length) / 4; i++) {
-            sõne.append(tähis).append(" ".repeat((kaardiKülg * 2) - 3)).append(tähis);
-        }*/
-
         for (String s : osad) {
             int tühikuidEnne = (kaardiKülg * 2 - 3 - s.length()) / 2;
             sõne.append(tähis + " ".repeat(((kaardiKülg * 2) - 3 - s.length()) / 2) + s +
                     " ".repeat(((kaardiKülg * 2) - 3 - s.length()) - tühikuidEnne) + tähis).append("\n");
         }
 
-        /*for (int i = 2; i < (kaardiKülg - osad.length) / 4; i++) {
-            sõne.append(tähis + " ".repeat((kaardiKülg * 2) - 3) + tähis);
-        }*/
         sõne.append(tähis2.repeat(kaardiKülg)).append("\n");
         return String.valueOf(sõne);
     }
